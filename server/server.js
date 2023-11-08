63d9e8d381f7f53c17c900618e7c8c49e197463f
Ravi Ranjan <2140131@sliet.ac.in>
2023-11-09 01:34:17 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const { log } = require('ravi-first');
+
+app.get('/', (req,res) => {
+    res.status(200).send("Welcome to new Project !");
+})
+
+app.get('/register', (req,res) => {
+    res.status(200).send("Welcome to registration page !");
+})
+
+const PORT = 3000;
+app.listen(PORT, ()=>{
+    log(`Server is running at PORT : ${PORT}`);
+})
