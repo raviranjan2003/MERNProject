9d72241a4ddadc1265b035cbde2f47d761076093
Ravi Ranjan <2140131@sliet.ac.in>
2023-11-11 22:38:59 +0530

    add auth-controllers

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,20 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const authControllers = require('../../controllers/auth-controller')
 
 router
 .get('/register', (req,res) => {
     res.status(200).send("Welcome to registration page ! --> GET");
 })
-.post('/register', (req,res) => {
-    res.status(200).send("Welcome to registration page ! --> POST");
-})
+.post('/register', authControllers.signUp );
 
 router
 .get('/login', (req,res) => {
     res.status(200).send("Welcome to login page ! --> GET");
 })
-.post('/login', (req,res) => {
-    res.status(200).send("Welcome to login page ! --> POST");
-})
+.post('/login', authControllers.signIn );
 
 module.exports = router;
