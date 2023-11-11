0a2fbd645fdd3fd5d9272ffffcb08dcb8c8714cf
Ravi Ranjan <2140131@sliet.ac.in>
2023-11-12 00:19:39 +0530

    modification in auth controller

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,4 +1,5 @@
 const signUp = async (req,res) => {
+    console.log(req.body);
     res.status(200).send("Welcome to registration page ! --> POST");
 }
 const signIn = async (req,res) => {
--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,17 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authControllers = require('../../controllers/auth-controller')
 
-router
-.get('/register', (req,res) => {
-    res.status(200).send("Welcome to registration page ! --> GET");
-})
-.post('/register', authControllers.signUp );
-
-router
-.get('/login', (req,res) => {
-    res.status(200).send("Welcome to login page ! --> GET");
-})
-.post('/login', authControllers.signIn );
+router.use('/auth', require("./auth-api"));
 
 module.exports = router;
