7dd4a8974f764f049472fff5b0a946844b9750e7
Ravi Ranjan <2140131@sliet.ac.in>
2023-11-12 23:39:05 +0530

    added .env files

---
+++ server/utils/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const URI = process.env.MONGODB_URI;
+
+const connectDb = async () => {
+    try {
+        await mongoose.connect(URI);
+        console.log("DB connected successfully !");
+    } catch (error) {
+        console.log("Db connection error => ", error);
+    }
+}
+
+module.exports = connectDb;
