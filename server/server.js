7a371990efe0f19b27b1c75ffa6888a39c6e6ac9
Ravi Ranjan <2140131@sliet.ac.in>
2023-11-12 23:37:54 +0530

    connected with database

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
+const connectDb = require('./utils/db');
 
 const { log } = require('ravi-first');
 
 app.use(express.json());
+
 app.use('/', require("./routes"))
 
 
 
 const PORT = 3000;
-app.listen(PORT, ()=>{
-    log(`Server is running at PORT : ${PORT}`);
+connectDb().then(()=>{
+    app.listen(PORT, ()=>{
+        log(`Server is running at PORT : ${PORT}`);
+    })
 })
